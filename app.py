0402e90ff33e6e1233cd232535d131e3ca8ce9df
nouris24-meet <nour.stety@gmail.com>
2023-07-30 16:16:56 +0300

    heere

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def chatroom():
             if not gc_name:
                 raise ValueError("Groupchat name not provided in the form.")
 
-            # Create or update the artist node with the 'gc_name' key
-            db.child('Groupchats').child(gc_name).set({"Groupchat_name": gc_name})
+            # Create or update the gc node with the 'gc_name' key
+            db.child('Groupchats').child(gc_name).set({"groupchat_name": gc_name})
 
-            return redirect(url_for('home', gc=gc_name))
+            return redirect(url_for('home', gc_name=gc_name))
         except Exception as e:
             print("Couldn't create group chat")
             print(e)
@@ -55,12 +55,26 @@ def chatroom():
         for item in groupchats:
             # Check if 'gc_name' key exists in the item dictionary
             if 'gc_name' in item:
-                gc_names.append(item['gc_name'])
+                groupchats_names.append(item['gc_name'])
             else:
-                print("Artist node doesn't have 'gc_name' key:", item)
+                print("Groupchat node doesn't have 'gc_name' key:", item)
 
     print(groupchats_names)
-    return render_template("chat.html", groupchats_names=groupchats_names)
+    return render_template("chatroom.html", groupchats_names=groupchats_names)
+
+@app.route('/ac_chat/<string:gc>', methods=['GET', 'POST'])
+def ac_chat(gc):
+    if request.method=='POST':
+        try:
+            me=request.form['message']
+            mes={'message':me}
+            db.child('Messages').child(gc).push(mes)            
+            chat = db.child('Messages').child(gc).get().val()
+            return render_template('ac_chat.html', gc=gc, message=me, chat=chat)
+        except:
+            print("Couldn't find a message ")
+    chat = db.child('Messages').child(gc).get().val()
+    return render_template('ac_chat.html', gc=gc,chat=chat)
 
 
 if __name__ == '__main__':
