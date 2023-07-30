d5a9d0a4806c6d1b36054c490cdbc04e399cdf4c
liemst24-meet <liem.strykowski@gmail.com>
2023-07-30 13:14:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+config = {
+  "apiKey": "AIzaSyDeCR2mPfW9yT__WZ0F90IEhSwjYU4TOi8",
+  "authDomain": "y2-groupc-team1.firebaseapp.com",
+  "projectId": "y2-groupc-team1",
+  "storageBucket": "y2-groupc-team1.appspot.com",
+  "messagingSenderId": "289049142886",
+  "appId": "1:289049142886:web:aa1fb5cf71fad3cb392fa7",
+  "databaseURL" : "https://y2-groupc-team1-default-rtdb.firebaseio.com/"
+}
 
 
+@app.route('/donations', methods=['GET', 'POST'])
+def donate():
+    return render_template('donations.html')
 
 
-#Code goes above here
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
